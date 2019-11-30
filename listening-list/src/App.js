694d1a164c92d6f3dd38faf6db965bae04c3a79d
Nick <nhartmusic@gmail.com>
2019-11-30 14:55:17 +0100

    initial app setup

---
+++ listening-list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Listening_List from './components/Listening_List'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Listening_List />
+    </div>
+  )
+}
+
+export default App
---
+++ listening-list/src/components/Listening_List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Listening_List extends Component {
+    render() {
+        return <div classname='listen'>
+            <ul>
+                <li>Mujeres</li>
+                <li>Chocolate and Cheese</li>
+                <li>False Priest</li>
+            </ul>
+        </div>
+    }
+}
+
+export default Listening_List
