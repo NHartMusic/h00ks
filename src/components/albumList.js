7858a55cf85463fc53723f088239fd46cb163848
Nick Hart <nhartmusic@gmail.com>
2020-06-29 23:18:07 +0200

    converting to functional components and using hooks

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,21 +1,19 @@
-import React, { Component, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/themeContext'
 
-class AlbumList extends Component {
-    static contextType = ThemeContext
-    render() {
-        const { isLightTheme, light, dark } = this.context
-        const theme = isLightTheme ? light : dark
-        return (
-            <div className='album-list' style={{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>When I Get Home</li>
-                    <li style={{ background: theme.ui }}>Phrenology</li>
-                    <li style={{ background: theme.ui }}>OK Computer</li>
-                </ul>
-            </div>
-        )
-    }
+const AlbumList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+    return (
+        <div className='album-list' style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>When I Get Home</li>
+                <li style={{ background: theme.ui }}>Phrenology</li>
+                <li style={{ background: theme.ui }}>OK Computer</li>
+            </ul>
+        </div>
+    )
 }
 
 export default AlbumList
+
