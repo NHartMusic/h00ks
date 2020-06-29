5e347524906c69a1ce9555a3e7605b6760ce78d6
Nick Hart <nhartmusic@gmail.com>
2020-06-29 16:43:27 +0200

    ok

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../contexts/themeContext'
 
 class AlbumList extends Component {
