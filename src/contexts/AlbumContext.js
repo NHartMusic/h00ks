15f84a375888e00685a41cd661ca4d61c0a9b789
Nick Hart <nhartmusic@gmail.com>
2020-06-30 18:13:18 +0200

    now saving things in local storage

--- src/contexts/AlbumContext.js
+++ src/contexts/AlbumContext.js
@@ -1,10 +1,16 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import { albumReducer } from '../reducers/albumReducer'
 
 export const AlbumContext = createContext()
 
 const AlbumContextProvider = (props) => {
-    const [albums, dispatch] = useReducer(albumReducer, [])
+    const [albums, dispatch] = useReducer(albumReducer, [], () => {
+        const localData = localStorage.getItem('albums')
+        return localData ? JSON.parse(localData) : []
+    })
+    useEffect(() => {
+        localStorage.setItem('albums', JSON.stringify(albums))
+    }, [albums])
     return (
         <AlbumContext.Provider value={{ albums, dispatch }}>
             {props.children}
